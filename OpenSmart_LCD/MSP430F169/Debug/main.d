f2d72323a8e58f09131c12bdcd7ee36e4ddc1dfc
yingshaoxo <yingshaoxo@gmail.com>
2019-09-22 18:25:04 +0800

    now we need to control LCD with MSP430F169

---
+++ OpenSmart_LCD/MSP430F169/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/yingshaoxo/ti/ccs910/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /home/yingshaoxo/ti/ccs910/ccs/ccs_base/msp430/include/msp430f169.h
+main.obj: /home/yingshaoxo/ti/ccs910/ccs/ccs_base/msp430/include/in430.h
+main.obj: /home/yingshaoxo/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics.h
+main.obj: /home/yingshaoxo/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+/home/yingshaoxo/ti/ccs910/ccs/ccs_base/msp430/include/msp430.h: 
+/home/yingshaoxo/ti/ccs910/ccs/ccs_base/msp430/include/msp430f169.h: 
+/home/yingshaoxo/ti/ccs910/ccs/ccs_base/msp430/include/in430.h: 
+/home/yingshaoxo/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics.h: 
+/home/yingshaoxo/ti/ccs910/ccs/tools/compiler/ti-cgt-msp430_18.12.2.LTS/include/intrinsics_legacy_undefs.h: 
---
+++ OpenSmart_LCD/MSP430F169/main.c
@@ -0,0 +1,59 @@
+#include <msp430f169.h> 
+
+int main(void)
+{
+    volatile unsigned int i;
+    WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+    P3SEL |= 0xC0;                            // P3.6 and P3.7 = USART1 option select; 3.6 TX, 3.7 RX
+
+    BCSCTL1 &= ~XT2OFF;                       // XT2on
+
+    do
+    {
+        IFG1 &= ~OFIFG;                           // Clear OSCFault flag
+        for (i = 0xFF; i > 0; i--);               // Time for flag to set
+    }
+    while ((IFG1 & OFIFG));                   // OSCFault flag still set?
+
+    BCSCTL2 |= SELM_2 + SELS;                 // MCLK= SMCLK= XT2 (safe)
+    ME2 |= UTXE1 + URXE1;                     // Enable USART1 TXD/RXD
+    UCTL1 |= CHAR;                            // 8-bit character
+    UTCTL1 |= SSEL1;                          // UCLK = SMCLK
+    UBR01 = 0x45;                             // 8Mhz/115200 - 69.44
+    UBR11 = 0x00;                             //
+    UMCTL1 = 0x2C;                            // modulation
+    UCTL1 &= ~SWRST;                          // Initialize USART state machine
+
+    IE2 |= URXIE1;                            // Enable USART1 RX interrupt
+
+    _BIS_SR(GIE); // just enable general interrupt
+}
+
+unsigned int BYTE_YOU_WANT_TO_SEND;
+unsigned int BYTE_FROM_SERIAL;
+#pragma vector=USART1RX_VECTOR
+__interrupt void usart1_rx (void)
+{
+    while (!(IFG2 & UTXIFG1));                // USART1 TX buffer ready?
+
+    //TXBUF1 = RXBUF1;                          // RXBUF1 to TXBUF1
+    BYTE_FROM_SERIAL = REXBUF1;
+    TXBUF1 = BYTE_YOU_WANT_TO_SEND;
+}
+
+unsigned char* hex_string_to_char(const char* hexstr)
+{
+    size_t len = strlen(hexstr);
+    IF_ASSERT(len % 2 != 0)
+        return NULL;
+    size_t final_len = len / 2;
+    unsigned char* chrs = (unsigned char*)malloc((final_len+1) * sizeof(*chrs));
+    for (size_t i=0, j=0; j<final_len; i+=2, j++)
+        chrs[j] = (hexstr[i] % 32 + 9) % 25 * 16 + (hexstr[i+1] % 32 + 9) % 25;
+    chrs[final_len] = '\0';
+    return chrs;
+}
+
+void OpenSmart_write_command() {
+    ;
+}
